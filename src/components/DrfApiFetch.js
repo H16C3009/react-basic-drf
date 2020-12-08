2bc02db4d9a6e8b06cf9b62df0088db0f6782217
yoshi <yoshiaki11205656@gmail.com>
2020-12-08 23:48:00 +0900

    delete task

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -26,7 +26,13 @@ const DrfApiFetch = () => {
         })
     }
 
-    console.log(tasks)
+    const deleteTask = () => {
+        Axios.delete(`http://127.0.0.1:8000/v1/api/tasks/${id}`, {
+            headers: {
+                'Authorization': 'Token fd92847e04d5748fa8d13489937c239b86d928d5'
+            }
+        }).then( res => console.log(res))
+    }
 
     return (
         <>
@@ -36,6 +42,13 @@ const DrfApiFetch = () => {
                         <li key={index}>{task.id} {task.title}</li>
                     ))
                 }
+                set id <br />
+                <input type='text' value={id} onChange={ e => setId(e.target.value) } />
+                <br />
+                <button onClick={getTask} >Get Task</button>
+                <br />
+                <button onClick={deleteTask} >Delete Task</button>
+            <h3>{selectedTask.title}</h3>
             </ul>
         </>
     )
