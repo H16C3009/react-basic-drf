f0ff52fb945275cfb5e79d8cf3fcb1e6ee1f7c11
yoshi <yoshiaki11205656@gmail.com>
2020-12-09 01:09:42 +0900

    create task

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 const DrfApiFetch = () => {
     const [ tasks, setTasks ] = useState([])
     const [ selectedTask, setSelectedTask ] = useState([])
+    const [ editedTask, setEditTask] = useState({ id: '', title: ''})
     const [ id, setId] = useState(1)
     
     useEffect(() => {
@@ -17,7 +18,7 @@ const DrfApiFetch = () => {
     }, [])
 
     const getTask = () => {
-        Axios.get(`http://127.0.0.1:8000/v1/api/tasks/${id}`, {
+        Axios.get(`http://127.0.0.1:8000/v1/api/tasks/${id}/`, {
             headers: {
                 'Authorization': 'Token fd92847e04d5748fa8d13489937c239b86d928d5'
             }
@@ -27,7 +28,7 @@ const DrfApiFetch = () => {
     }
 
     const deleteTask = ( taskId ) => {
-        Axios.delete(`http://127.0.0.1:8000/v1/api/tasks/${taskId}`, {
+        Axios.delete(`http://127.0.0.1:8000/v1/api/tasks/${taskId}/`, {
             headers: {
                 'Authorization': 'Token fd92847e04d5748fa8d13489937c239b86d928d5'
             }
@@ -35,6 +36,27 @@ const DrfApiFetch = () => {
             setTasks(tasks.filter(task => task.id !== taskId))
         )
         setSelectedTask([])
+    }
+
+    const createTask = ( task ) => {
+        const data = {
+            title: task.title
+        }
+        Axios.post('http://127.0.0.1:8000/v1/api/tasks/', data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token fd92847e04d5748fa8d13489937c239b86d928d5'
+            }
+        }).then( res => 
+            setTasks([ ...tasks, res.data ])
+        )
+        setSelectedTask([])
+    }
+
+    const handleInputChange = () => e => {
+        const value = e.target.value
+        const name = e.target.name
+        setEditTask({ ...editedTask, [name]: value })
     }
 
     return (
@@ -54,6 +76,10 @@ const DrfApiFetch = () => {
                 <br />
                 <button onClick={() => getTask()} >Get Task</button>
                 <h3>{selectedTask.title}</h3>
+                <br />
+                <input type='text' name='title' value={editedTask.title}
+                 onChange={ handleInputChange() } required />
+                <button onClick={ () => createTask(editedTask) }>Create Task</button>
             </ul>
         </>
     )
